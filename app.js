f4cddc437e9767a7be7118453fb582d5947b3401
Bello Roqeeb <roqbell@gmail.com>
2022-09-11 10:03:03 +0100

    getting it right

---
+++ app.js
@@ -0,0 +1,19 @@
+let penne = 16.68;
+console.log(penne);
+console.log("Love");
+document.write("oba");
+
+
+
+
+
+
+var card = document.querySelector("#card");
+
+function openRegister() {
+    card.style.transform = "rotateY(-180deg)";
+}
+
+function openLogin() {
+    card.style.transform = "rotateY(0deg)";
+}
